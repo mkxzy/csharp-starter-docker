513dc5aa81dee36308611e1e91cb08adb6d370a0
Mike <1715380210@qq.com>
2022-04-21 00:03:00 +0800

    'Initial'

---
+++ csharp-starter-docker/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_starter_docker.Controllers
+{
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [Route("/")]
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello, World!";
+        }
+    }
+}
